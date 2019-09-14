fe851ed92ef09efe7c04ca5fb8ea5d424c8726e1
Ren Dog <rene.berlemont@gmail.com>
2019-09-14 14:10:29 +0100

    added models.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
-from werkzeug.utils import import_string
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import import_string
 
 app = Flask(__name__)
 cfg = import_string(os.environ['APP_SETTINGS'])()
 app.config.from_object(cfg)
+db = SQLAlchemy(app)
+
+from models import User
 
 @app.route('/')
 def hello():
