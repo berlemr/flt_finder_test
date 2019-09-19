5c547c2b5b79f4f45eefdb5ae7b19be67d05fd87
Ren Dog <rene.berlemont@gmail.com>
2019-09-19 20:56:45 +0100

    separated out models from app

--- app.py
+++ app.py
@@ -2,23 +2,19 @@ import os
 import time
 
 from flask import Flask, render_template, flash, redirect, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import import_string
 
 from forms import LoginForm
+from models import db, Destination
 
 os.environ['APP_SETTINGS'] = 'config.DevelopmentConfig' #just for convenience, remove when finished development
 
 app = Flask(__name__)
 cfg = import_string(os.environ['APP_SETTINGS'])()
 app.config.from_object(cfg)
-db = SQLAlchemy(app)
 
-class Destination(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(80), unique=True, nullable=False)
+db.init_app(app)  # call init_app here instead of db = SQLAlchemy(app)
 
-NAMES = [i.city for i in Destination.query.all()]
 
 @app.route('/')
 def index():
@@ -33,6 +29,7 @@ def test():
 def autocomplete():
     search = request.args.get('term')
     app.logger.debug(search)
+    NAMES = Destination.get_all_cities()
     return jsonify(json_list=NAMES)
 
 @app.route('/login', methods=['GET', 'POST'])
--- models.py
+++ models.py
@@ -1,4 +1,14 @@
-from app import db
+#from app import db
+# from db_util import db
+# from app import app
+# from flask_sqlalchemy import SQLAlchemy
+#
+# db = SQLAlchemy(app)
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()  # NOTE: app not imported from app.py and passed in
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +21,7 @@ class User(db.Model):
 class Destination(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(80), unique=True, nullable=False)
+
+    @staticmethod
+    def get_all_cities():
+        return [i.city for i in Destination.query.all()]
